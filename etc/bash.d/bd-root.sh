385dc0321b4a68cebe3637ba0dec061569932302
jtingiris@d0 <joseph.tingiris@gmail.com>
2023-06-03 13:41:30 -0400

    moved

--- etc/bash.d/bd-root.sh
+++ etc/bash.d/bd-root.sh
@@ -1,4 +1,4 @@
-# bd-root.sh: appropriately add bd-root and bd-root-profile aliases to a shell's environment
+# bd-root.sh: appropriately add bd-root and bd-root-login aliases to a shell's environment
 
 # Copyright (C) 2018-2023 Joseph Tingiris <joseph.tingiris@gmail.com>
 # https://github.com/bash-d/bd/blob/main/LICENSE.md
@@ -47,18 +47,18 @@ if [ "${USER}" != "root" ]; then
                 BD_ROOT_SUDO_MUST_PRESERVE_ENV="${BD_ROOT_SUDO_MUST_PRESERVE_ENV//,,/,}"
 
                 alias bd-root="${BD_ROOT_SUDO_BIN} --preserve-env=${BD_ROOT_SUDO_MUST_PRESERVE_ENV} -u root ${BD_ROOT_BASH_BIN} --init-file ${BD_BASH_INIT_FILE}"
-                alias bd-root-profile="${BD_ROOT_SUDO_BIN} --preserve-env=${BD_ROOT_SUDO_MUST_PRESERVE_ENV} -u root --login"
+                alias bd-root-login="${BD_ROOT_SUDO_BIN} --preserve-env=${BD_ROOT_SUDO_MUST_PRESERVE_ENV} -u root --login"
 
                 unset -v BD_ROOT_SUDO_MUST_PRESERVE_ENV
             else
                 if [ ${#BD_ROOT_SU_BIN} -gt 0 ] && [ -x "${BD_ROOT_SU_BIN}" ]; then
                     alias bd-root="su --login root -c '${BD_ROOT_BASH_BIN} --init-file ${BD_BASH_INIT_FILE}'"
-                    alias bd-root-profile='su --login'
+                    alias bd-root-login='su --login'
                 fi
             fi
         fi
     fi
 else
     alias bd-root='source "${BD_BASH_INIT_FILE}"'
-    alias bd-root-profile=bd-root
+    alias bd-root-login=bd-root
 fi
